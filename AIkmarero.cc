5342b3b2dfbeca4ec23181a25016f1a4a185073b
polo3cat <polo3ies@gmail.com>
2016-05-16 09:19:19 +0200

    Cambiado stack por vector, se accede con iterator

--- AIkmarero.cc
+++ AIkmarero.cc
@@ -31,50 +31,23 @@ struct PLAYER_NAME : public Player {
    int r = rows();
    int c = cols();
    bool buscar = true;
-   stack<Dir> recorrido; //El camino que habrá que seguir
+   vector<Dir> recorrido; //El camino que habrá que seguir
    Pos destino;
+   vector<Dir>::iterator trace; //nos indica donde buscar en el vector que contiene las direcciones
 
-    /**
-     * Play method.
-     * 
-     * This method will be invoked once per each round.
-     * You have to read the board here to place your actions
-     * for this round.
-     *
-     */          
+   ////////////////////////////////
+   //////////FUNCIONES/////////////
+   ////////////////////////////////
 
-    virtual void play () {
-        const Poquemon& yo = poquemon(me());
-        if(yo.alive) {
-            Pos posActual = yo.pos;
-            if (buscar) {
-                while (not recorrido.empty()) {
-                    recorrido.pop();                   
-                }
-                destino = busqueda(posActual, Point, recorrido);
-                buscar = false;
-            }
-            if(points_value(destino) <= 0) {
-                buscar = true;
-            } 
-            if(not recorrido.empty()) {
-                Dir d;
-                d = recorrido.top();
-                recorrido.pop();
-                Move(d);                
-            }
-        }
-    }
-
-    Pos busqueda(Pos posActual, CType& tipo, stack<Dir>& recorridoFinal){
+   Pos busqueda(Pos posActual, CType tipo, vector<Dir>& recorridoFinal){
         vector<vector<bool> > mVisitado(r, vector<bool>(c, false));
         vector<vector<int> > mDistancia(r, vector<int>(c, -1));
-        vector<vector<Pos> > mPadre(r, vector<Dir>(c));
+        vector<vector<Dir> > mPadre(r, vector<Dir>(c, None));
 
         //iniciamos la busqueda en la posActual
         mVisitado[posActual.i][posActual.j] = true;
         mDistancia[posActual.i][posActual.j] = 0;
-        mPadre[posActual.i][posActual.j] = NULL;
+        mPadre[posActual.i][posActual.j] = None;
         queue<Pos> colaBFS;
         colaBFS.push(posActual);
         bool found = false;
@@ -87,7 +60,7 @@ struct PLAYER_NAME : public Player {
             posActual = colaBFS.front();
             colaBFS.pop();
             posAux = posActual + Left;
-            if (not mVisitado[posAux.i][posAux.j] and (cell_type(posAux) != Wall or (cell_type(posAux) == Wall and ghost_wall(posAux) > mDistancia[posActual.i][posActual.j] ) ) ) {
+            if (pos_ok(posAux) and not mVisitado[posAux.i][posAux.j] and (cell_type(posAux) != Wall or (cell_type(posAux) == Wall and ghost_wall(posAux) > mDistancia[posActual.i][posActual.j] ) ) ) {
                 mVisitado[posAux.i][posAux.j] = true;
                 mDistancia[posAux.i][posAux.j] = mDistancia[posActual.i][posActual.j] + 1;
                 mPadre[posAux.i][posAux.j] = Right;
@@ -100,7 +73,7 @@ struct PLAYER_NAME : public Player {
             }
 
             posAux = posActual + Top;
-            if (not mVisitado[posAux.i][posAux.j] and (cell_type(posAux) != Wall or (cell_type(posAux) == Wall and ghost_wall(posAux) > mDistancia[posActual.i][posActual.j] ) ) ) {
+            if (pos_ok(posAux) and not mVisitado[posAux.i][posAux.j] and (cell_type(posAux) != Wall or (cell_type(posAux) == Wall and ghost_wall(posAux) > mDistancia[posActual.i][posActual.j] ) ) ) {
                 mVisitado[posAux.i][posAux.j] = true;
                 mDistancia[posAux.i][posAux.j] = mDistancia[posActual.i][posActual.j] + 1;
                 mPadre[posAux.i][posAux.j] = Bottom;
@@ -113,7 +86,7 @@ struct PLAYER_NAME : public Player {
             }
 
             posAux = posActual + Right;
-            if (not mVisitado[posAux.i][posAux.j] and (cell_type(posAux) != Wall or (cell_type(posAux) == Wall and ghost_wall(posAux) > mDistancia[posActual.i][posActual.j] ) ) ) {
+            if (pos_ok(posAux) and not mVisitado[posAux.i][posAux.j] and (cell_type(posAux) != Wall or (cell_type(posAux) == Wall and ghost_wall(posAux) > mDistancia[posActual.i][posActual.j] ) ) ) {
                 mVisitado[posAux.i][posAux.j] = true;
                 mDistancia[posAux.i][posAux.j] = mDistancia[posActual.i][posActual.j] + 1;
                 mPadre[posAux.i][posAux.j] = Left;
@@ -126,7 +99,7 @@ struct PLAYER_NAME : public Player {
             }
 
             posAux = posActual + Bottom;
-            if (not mVisitado[posAux.i][posAux.j] and (cell_type(posAux) != Wall or (cell_type(posAux) == Wall and ghost_wall(posAux) > mDistancia[posActual.i][posActual.j] ) ) ) {
+            if (pos_ok(posAux) and not mVisitado[posAux.i][posAux.j] and (cell_type(posAux) != Wall or (cell_type(posAux) == Wall and ghost_wall(posAux) > mDistancia[posActual.i][posActual.j] ) ) ) {
                 mVisitado[posAux.i][posAux.j] = true;
                 mDistancia[posAux.i][posAux.j] = mDistancia[posActual.i][posActual.j] + 1;
                 mPadre[posAux.i][posAux.j] = Top;
@@ -138,14 +111,55 @@ struct PLAYER_NAME : public Player {
             }
         }
         //guardamos el recorrido en una pila
-        ret = posEncontrado;
+        Pos ret = posEncontrado;
         while(posEncontrado != posInicial) {
-            recorridoFinal.push(mPadre[posEncontrado.i][posEncontrado.j]);
+            if (trace != recorridoFinal.end()) {
+                *trace = mPadre[posEncontrado.i][posEncontrado.j];
+                ++trace;
+            }
+            else {
+                recorridoFinal.push_back(mPadre[posEncontrado.i][posEncontrado.j]);
+                trace = recorridoFinal.end() - 1;
+            }
             posEncontrado = posEncontrado + mPadre[posEncontrado.i][posEncontrado.j];
         }
         return ret;
 
     }
+    /**
+     * Play method.
+     * 
+     * This method will be invoked once per each round.
+     * You have to read the board here to place your actions
+     * for this round.
+     *
+     */          
+
+    virtual void play () {
+        const Poquemon& yo = poquemon(me());
+        Action accion;
+        if(yo.alive) {
+            Pos posActual = yo.pos;
+            if (buscar) {
+                trace = recorrido.begin();
+                destino = busqueda(posActual, Point, recorrido);
+                buscar = false;
+            }
+            if(points_value(destino) <= 0) {
+                buscar = true;
+            } 
+            if(trace != recorrido.begin()) {
+                Dir d;
+                d = *trace;
+                --trace;
+                if(trace == recorrido.begin()) 
+                    buscar = true;
+                accion.move(d);                
+            }
+        }
+    }
+
+    
                 
 };
 
